072900b6430aa2f6260d2bc8e05ebbf4e1948539
mohira <shukatsudaison@gmail.com>
2019-05-31 21:21:49 +0900

    Add: #2 文字列表現を取得できる

--- test_semver.py
+++ test_semver.py
@@ -3,12 +3,21 @@ import unittest
 
 class SemVer:
     def __init__(self, major: int, minor: int, patch: int):
-        pass
+        self.major = major
+        self.minor = minor
+        self.patch = patch
+
+    def __str__(self):
+        return f"{self.major}.{self.minor}.{self.patch}"
 
 
 class TestSemVer(unittest.TestCase):
-    def test_バージョンオブジェクトを生成できる(self):
-        SemVer(major=1, minor=4, patch=2)
+    def test_バージョンオブジェクトの文字列表現を取得できる(self):
+        with self.subTest("1.4.2"):
+            self.assertEqual("1.4.2", str(SemVer(1, 4, 2)))
+
+        with self.subTest("1.5.8"):
+            self.assertEqual("1.5.8", str(SemVer(1, 5, 8)))
 
 
 if __name__ == "__main__":
